41e6d4e20db051909a5801a61c2f12eb5adc6528
timofei <tima.bratskov05@mail.ru>
2025-05-21 17:26:51 +0300

    config openapi

--- user_service/src/main.py
+++ user_service/src/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from src.users.router import router
+from src.openapi_config import configure_swagger
 
 
 app = FastAPI()
 app.include_router(router)
+
+configure_swagger(app)
---
+++ user_service/src/openapi_config.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from fastapi.openapi.utils import get_openapi
+
+def configure_swagger(app: FastAPI):
+    def custom_openapi():
+        if app.openapi_schema:
+            return app.openapi_schema
+
+        openapi_schema = get_openapi(
+            title="Library User Service",
+            version="1.0.0",
+            description="JWT авторизация для пользователей",
+            routes=app.routes,
+        )
+        openapi_schema["components"]["securitySchemes"] = {
+            "BearerAuth": {
+                "type": "http",
+                "scheme": "bearer",
+                "bearerFormat": "JWT",
+            }
+        }
+        for path in openapi_schema["paths"].values():
+            for method in path.values():
+                method["security"] = [{"BearerAuth": []}]
+        app.openapi_schema = openapi_schema
+        return app.openapi_schema
+
+    app.openapi = custom_openapi
