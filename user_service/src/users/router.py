842dc02b9ea84f73a06ccd5eecf3b1283620d89f
timofei <tima.bratskov05@mail.ru>
2025-05-21 18:28:59 +0300

    ref router and service

--- user_service/src/users/router.py
+++ user_service/src/users/router.py
@@ -3,7 +3,10 @@ from uuid import UUID
 from src.users.service import UserService
 from src.users.schemas import  UserResponse, UserRequest, Token
 from src.dependencies import get_user_service
-
+from src.users.models import UserRole
+from src.auth import security
+from authx import RequestToken
+import traceback
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
@@ -14,7 +17,11 @@ async def create_user(
     service: UserService = Depends(get_user_service),
 ):
     try:
-        token = await service.create_user(user_data)
+        user = await service.create_user(user_data)
+        token = security.create_access_token(
+            uid=str(user.id),
+            role=user.role
+        )
         return {"access_token": token, "token_type": "bearer"}
     except ValueError as e:
         raise HTTPException(400, detail=str(e))
@@ -22,26 +29,30 @@ async def create_user(
 
 @router.delete("/{user_id}", status_code=204)
 async def delete_user(
-    user_id: UUID,
+    token: RequestToken = Depends(security.access_token_required),
     service: UserService = Depends(get_user_service)
 ):
     try:
-        await service.delete_user(user_id)
+        await service.delete_user(token.sub)
     except ValueError:
         raise HTTPException(404, detail="User not found")
 
-@router.get("/me/{id}", response_model=UserResponse)
+
+@router.get("/me", response_model=UserResponse)
 async def get_me(
-    id:UUID,
+    token: RequestToken = Depends(security.access_token_required),
     service: UserService = Depends(get_user_service)
 ):
     try:
-        user = await service.get_user(id)
-        return UserResponse(id=user.id, email=user.email)
+        return await service.get_user(UUID(token.sub))
+        
     except HTTPException as e:
         raise e
-    except Exception:
-        raise HTTPException(401, detail="Invalid token")
+    except Exception as e:
+        print("Ошибка при получении пользователя:")
+        traceback.print_exc()  # Вывод полного трейсбэка
+        raise HTTPException(status_code=401, detail=f"Invalid token or user not found: {e}")
+
 
 @router.post("/login")
 async def login(
@@ -49,7 +60,11 @@ async def login(
     service: UserService = Depends(get_user_service)
 ):
     try:
-        token = await service.login(user_creds)
+        user = await service.login(user_creds)
+        token = security.create_access_token(
+            uid=str(user.id),
+            role=user.role
+        )
         return {"access_token": token, "token_type": "bearer"}
     except ValueError as e:
         raise HTTPException(401, detail=str(e))
--- user_service/src/users/service.py
+++ user_service/src/users/service.py
@@ -1,9 +1,8 @@
 from src.users.repository import IUserRepository 
-from src.users.schemas import UserResponse, UserRequest
+from src.users.schemas import UserResponse, UserRequest,UserRole
 from src.users.models import UserModel
 from uuid import UUID
 from passlib.context import CryptContext
-
 
 class UserService:
     def __init__(self, repo: IUserRepository):
@@ -11,40 +10,50 @@ class UserService:
         self._pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
-    async def create_user(self, user_req: UserRequest) -> str:
+    async def create_user(self, user_req: UserRequest) -> UserResponse:
         if await self._repo.get_by_email(user_req.email):
             raise ValueError("Email already exists")
         
         user_model = UserModel(
             email=user_req.email,
-            password=self._pwd_context.hash(user_req.password)
+            password=user_req.password 
             )
-        await self._repo.create(user_model)
-        return "token"
-        
+        user =  await self._repo.create(user_model)
+        return UserResponse(
+            id=user.id,                
+            email=user.email,
+            role=UserRole.user
+            )
+        #  password=self._pwd_context.hash(user_req.password)
+
 
     async def delete_user(self, id: UUID) -> bool:
-
             deleted_count = await self._repo.delete(id)
             if deleted_count == 0:
                 raise ValueError("User not found")
             return True
         
 
-    async def get_user(self, id:UUID) -> UserResponse:
+    async def get_user(self, id: UUID) -> UserResponse:
             user = await self._repo.get_by_id(id)
             if not user:
                 raise ValueError("User not found")
-            return user
+            return UserResponse(
+                 id=user.id,
+                 email=user.email,
+                 role=user.role
+            )
         
 
-    async def login(self, user_req: UserRequest) -> str:
+    async def login(self, user_req: UserRequest) -> UserResponse:
         user = await self._repo.get_by_email(user_req.email)
-        
         if not user:
             raise ValueError("User not found")
         if user.password != user_req.password:
             raise ValueError("Invalid credentials")
-        
-        return "gener token"
+        return UserResponse(
+            id=user.id,
+            email=user.email,
+            role=user.role
+        )
             
